fa9808c7a93770ccb300119651a5fc34b817d3c4
Eric Gan <eric@ercgn.com>
2014-02-24 01:18:36 -0500

    General-purpose parser working

--- src/main.py
+++ src/main.py
@@ -1,1 +1,34 @@
 import nltk
+import sys
+from parser import MyHTMLParser
+
+def driver(articlefd, testfd):
+    
+    if testfd == "":
+        print "Warning: Test file not implemented yet!"
+    
+    Parser = MyHTMLParser()
+    buf = articlefd.readline()
+    while buf != "":
+        Parser.feed(buf)
+        buf = articlefd.readline()
+    
+    print Parser.grabText()
+
+if __name__ == '__main__':
+    
+    #CURRENT USAGE: "python main.py [articleFile]"
+    #This block of code will need to change if we modify the usage. 
+    if len(sys.argv) != 2:
+        print "    Usage:",
+        print "python %s [articleFile]" % (sys.argv[0])
+        print "    Ex. python %s ../data/set4/a1.htm" % (sys.argv[0])
+        exit(0)
+    
+    ARTICLE_FILE = sys.argv[1]
+    articlefd = open(ARTICLE_FILE, "r")
+    testfd = "" #To be modified
+    
+    driver(articlefd, testfd)
+    
+    articlefd.close()
--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,30 @@
 from HTMLParser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        
+        #This is to be modified depending on the type of data we want to fetch
+        self.tagsToRead = ['p', 'ul', 'blockquote', 'dl', 'ol']
+        
+        #Other initialized variables
+        self.tagList = []
+        self.articleText = ""
+
+    def handle_starttag(self, tag, attrs):
+        self.tagList.append(tag)
+        
+    def handle_endtag(self, tag):
+        if self.tagList.pop() != tag:
+            raise "Error! Tag mismatch! Aborting..."
+    
+    def handle_data(self, data):
+        for tag in self.tagsToRead:
+            if tag in self.tagList:
+                self.articleText += data
+    
+    #Call this function to grab the article text.
+    def grabText(self):
+        return self.articleText
+    
     
